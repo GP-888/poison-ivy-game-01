ba3f38ba1c0db3325cff313db679e783fc0d7544
GP-888 <gail.parsloe@dragonflymountain.com>
2025-06-05 12:44:31 -0400

    First commit. Has basic tabs app with initial swipe demo on a real photo. Moving photo functionality not correct yet (currently tracks mouse after you click on photo).

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+import { Gesture, GestureController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  standalone: false
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
+  @ViewChild('swipeCard', { read: ElementRef }) swipeCard!: ElementRef;
 
-  constructor() {}
+  constructor(private gestureCtrl: GestureController) {}
 
+  ngAfterViewInit() {
+    const gesture: Gesture = this.gestureCtrl.create({
+      el: this.swipeCard.nativeElement,
+      gestureName: 'swipe',
+      threshold: 10,
+      onStart: () => {
+        this.swipeCard.nativeElement.style.transition = 'none';
+      },
+      onMove: ev => {
+        this.swipeCard.nativeElement.style.transform = `translateX(${ev.deltaX}px)`;
+      },
+      onEnd: ev => {
+        this.swipeCard.nativeElement.style.transition = 'transform 0.3s ease';
+        if (ev.deltaX > 150) {
+          console.log('Swiped right');
+          this.swipeCard.nativeElement.style.transform = 'translateX(1000px)';
+        } else if (ev.deltaX < -150) {
+          console.log('Swiped left');
+          this.swipeCard.nativeElement.style.transform = 'translateX(-1000px)';
+        } else {
+          this.swipeCard.nativeElement.style.transform = 'translateX(0)';
+        }
+      }
+    });
+
+    gesture.enable(true);
+  }
 }
